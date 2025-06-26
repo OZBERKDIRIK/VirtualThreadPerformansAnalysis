31a25aab246121c736e11e09bfc3494a36504071
ozberk.dirik <ozberk.dirik@tubitak.gov.tr>
2025-06-26 14:00:10 +0300

    Network package altında servera 5000 protundan bağlanması için client eklendi

--- src/main/java/network/Client.java
+++ src/main/java/network/Client.java
@@ -1,10 +1,29 @@
 package main.java.network;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 5000);
+             BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader serverInput = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
+            System.out.println("Sunucuya bağlandı. Mesaj yazın:");
+
+            String userInput;
+            while ((userInput = consoleInput.readLine()) != null) {
+                out.println(userInput);
+                String response = serverInput.readLine();
+                System.out.println("Sunucu: " + response);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
