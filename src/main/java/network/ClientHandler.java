00ee664636c538cd90de9f89431458704c8057ed
ozberk.dirik <ozberk.dirik@tubitak.gov.tr>
2025-06-26 14:15:51 +0300

    Network package altında servera 5000 protundan bağlanması için client eklendi

---
+++ src/main/java/network/ClientHandler.java
@@ -0,0 +1,30 @@
+package main.java.network;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (
+                BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
+            String message;
+            while ((message = input.readLine()) != null) {
+                System.out.println("Client'tan gelen: " + message);
+                output.println("Sunucu cevabı: " + message.toUpperCase()); // basit cevap mekanizması
+            }
+        } catch (Exception e) {
+            System.out.println("Bağlantı hatası: " + e.getMessage());
+        }
+    }
+}
